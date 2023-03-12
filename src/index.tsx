441206a079a9df16953465c3839bef81fc2dccef
Samfenn <samir.fennikh@gmail.com>
2023-03-12 20:12:30 +0100

    Create Redirect component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home/Home';
+import { Redirect } from './pages/redirect/Redirect';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:shortId' element={<Redirect />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/redirect/Redirect.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const Redirect = () => {
+  const { shortId } = useParams();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetchLink();
+  }, []);
+
+  // If short link exist, redirect to the origin url
+  const fetchLink = async () => {
+    const response = await axios.get(`${API_URL}/links/${shortId}`);
+    if (response.status === 200) {
+      window.location.href = `${API_URL}/redirect/${shortId}`;
+    } else {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <span>
+      Sorry, the link you accessed doesn't exist. Please keep in mind that our shortened links are case sensitive and may contain letters and numbers.
+    </span>
+  );
+};
